695dc00c97497bd897af3280d12c94473d48fbcc
Rodrigo Santos <rsantosrobledo@gmail.com>
2026-09-23 15:15:31 +0200

    feat: make eye clones appear with instantaneous hard cut without scale animation

--- src/components/IntroSequence/eyeClones.js
+++ src/components/IntroSequence/eyeClones.js
@@ -1,555 +1,579 @@
-// Randomly and chaotically distributed eye clones (no spiral, no clear pattern)
+// Randomly distributed eye clones with hard-cut appearance
 export const EYE_CLONES = [
   {
-    "tx": "-39.4vw",
-    "ty": "37.4vh",
+    "tx": "11.0vw",
+    "ty": "-5.5vh",
+    "scale": 1.13,
+    "delay": 0.318
+  },
+  {
+    "tx": "-25.2vw",
+    "ty": "-33.2vh",
+    "scale": 1.09,
+    "delay": 0.057
+  },
+  {
+    "tx": "-12.7vmin",
+    "ty": "34.2vmin",
+    "scale": 1.25,
+    "delay": 0.357
+  },
+  {
+    "tx": "-5.6vmin",
+    "ty": "2.0vmin",
+    "scale": 1.06,
+    "delay": 0.349
+  },
+  {
+    "tx": "1.7vmin",
+    "ty": "8.5vmin",
+    "scale": 1.11,
+    "delay": 0.127
+  },
+  {
+    "tx": "-43.4vw",
+    "ty": "-20.7vh",
+    "scale": 1.21,
+    "delay": 0.018
+  },
+  {
+    "tx": "13.9vmin",
+    "ty": "-8.4vmin",
+    "scale": 0.97,
+    "delay": 0.026
+  },
+  {
+    "tx": "28.7vw",
+    "ty": "-39.0vh",
     "scale": 1.14,
-    "delay": 0.208
+    "delay": 0.232
   },
   {
-    "tx": "-12.5vw",
-    "ty": "5.8vh",
-    "scale": 0.98,
-    "delay": 0.073
+    "tx": "42.9vw",
+    "ty": "-21.2vh",
+    "scale": 1.28,
+    "delay": 0.076
   },
   {
-    "tx": "36.5vmin",
-    "ty": "-7.6vmin",
-    "scale": 1.19,
-    "delay": 0.09
+    "tx": "-19.6vw",
+    "ty": "-47.0vh",
+    "scale": 1.25,
+    "delay": 0.209
   },
   {
-    "tx": "20.2vmin",
-    "ty": "-18.0vmin",
-    "scale": 1.05,
+    "tx": "-36.3vw",
+    "ty": "-22.3vh",
+    "scale": 0.92,
     "delay": 0.088
   },
   {
-    "tx": "1.9vw",
-    "ty": "40.1vh",
-    "scale": 0.92,
-    "delay": 0.204
+    "tx": "46.3vw",
+    "ty": "0.2vh",
+    "scale": 1.06,
+    "delay": 0.103
   },
   {
-    "tx": "23.7vmin",
-    "ty": "-13.6vmin",
-    "scale": 0.99,
+    "tx": "10.3vmin",
+    "ty": "-29.6vmin",
+    "scale": 1.13,
+    "delay": 0.205
+  },
+  {
+    "tx": "-9.6vw",
+    "ty": "24.5vh",
+    "scale": 1.14,
+    "delay": 0.111
+  },
+  {
+    "tx": "-47.8vw",
+    "ty": "-45.8vh",
+    "scale": 1.15,
+    "delay": 0.298
+  },
+  {
+    "tx": "-2.3vmin",
+    "ty": "21.7vmin",
+    "scale": 0.92,
     "delay": 0.029
   },
   {
-    "tx": "35.1vw",
-    "ty": "-9.2vh",
-    "scale": 0.97,
-    "delay": 0.045
+    "tx": "-1.6vw",
+    "ty": "10.4vh",
+    "scale": 1.03,
+    "delay": 0.049
   },
   {
-    "tx": "-29.3vmin",
-    "ty": "0.3vmin",
-    "scale": 0.94,
-    "delay": 0.126
+    "tx": "16.4vmin",
+    "ty": "25.2vmin",
+    "scale": 1.03,
+    "delay": 0.158
   },
   {
-    "tx": "-26.7vmin",
-    "ty": "3.0vmin",
-    "scale": 0.98,
-    "delay": 0.144
-  },
-  {
-    "tx": "-9.1vw",
-    "ty": "-26.0vh",
-    "scale": 1.0,
-    "delay": 0.141
-  },
-  {
-    "tx": "-27.9vmin",
-    "ty": "1.8vmin",
-    "scale": 1.22,
-    "delay": 0.052
-  },
-  {
-    "tx": "-11.9vw",
-    "ty": "39.4vh",
-    "scale": 0.93,
-    "delay": 0.007
-  },
-  {
-    "tx": "41.8vw",
-    "ty": "19.0vh",
+    "tx": "16.6vmin",
+    "ty": "-2.5vmin",
     "scale": 1.11,
-    "delay": 0.038
+    "delay": 0.337
   },
   {
-    "tx": "7.2vmin",
-    "ty": "-22.3vmin",
-    "scale": 1.08,
-    "delay": 0.082
+    "tx": "14.9vw",
+    "ty": "38.3vh",
+    "scale": 1.07,
+    "delay": 0.38
   },
   {
-    "tx": "-42.4vw",
-    "ty": "14.1vh",
-    "scale": 1.21,
-    "delay": 0.023
+    "tx": "-45.1vw",
+    "ty": "44.4vh",
+    "scale": 1.18,
+    "delay": 0.228
   },
   {
-    "tx": "-17.5vw",
-    "ty": "-46.1vh",
-    "scale": 1.02,
-    "delay": 0.162
-  },
-  {
-    "tx": "-0.9vw",
-    "ty": "-10.1vh",
-    "scale": 1.19,
-    "delay": 0.214
-  },
-  {
-    "tx": "15.1vmin",
-    "ty": "-7.2vmin",
+    "tx": "1.2vw",
+    "ty": "-38.7vh",
     "scale": 1.15,
-    "delay": 0.031
+    "delay": 0.306
   },
   {
-    "tx": "-0.4vw",
-    "ty": "24.3vh",
-    "scale": 0.94,
-    "delay": 0.19
+    "tx": "-10.3vw",
+    "ty": "4.6vh",
+    "scale": 1.04,
+    "delay": 0.1
   },
   {
-    "tx": "22.5vw",
-    "ty": "44.3vh",
-    "scale": 1.26,
-    "delay": 0.22
-  },
-  {
-    "tx": "40.0vw",
-    "ty": "38.7vh",
-    "scale": 0.94,
-    "delay": 0.197
-  },
-  {
-    "tx": "0.0vmin",
-    "ty": "-27.6vmin",
-    "scale": 1.11,
-    "delay": 0.16
-  },
-  {
-    "tx": "26.5vw",
-    "ty": "6.5vh",
-    "scale": 1.15,
-    "delay": 0.142
-  },
-  {
-    "tx": "-1.4vmin",
-    "ty": "-11.0vmin",
-    "scale": 1.14,
-    "delay": 0.01
-  },
-  {
-    "tx": "-25.2vmin",
-    "ty": "-17.7vmin",
-    "scale": 1.06,
-    "delay": 0.096
-  },
-  {
-    "tx": "48.0vw",
-    "ty": "-45.3vh",
-    "scale": 1.05,
-    "delay": 0.18
-  },
-  {
-    "tx": "-13.9vw",
-    "ty": "-4.9vh",
-    "scale": 0.89,
-    "delay": 0.167
-  },
-  {
-    "tx": "12.2vmin",
-    "ty": "-32.1vmin",
-    "scale": 1.1,
-    "delay": 0.122
-  },
-  {
-    "tx": "11.6vw",
-    "ty": "-8.0vh",
-    "scale": 1.02,
-    "delay": 0.215
-  },
-  {
-    "tx": "16.7vw",
-    "ty": "-25.1vh",
-    "scale": 1.17,
-    "delay": 0.091
-  },
-  {
-    "tx": "3.9vmin",
-    "ty": "-9.0vmin",
-    "scale": 1.12,
-    "delay": 0.211
-  },
-  {
-    "tx": "18.8vmin",
-    "ty": "6.0vmin",
-    "scale": 1.15,
-    "delay": 0.032
-  },
-  {
-    "tx": "0.2vw",
-    "ty": "-37.3vh",
-    "scale": 0.99,
-    "delay": 0.215
-  },
-  {
-    "tx": "-11.8vmin",
-    "ty": "-7.1vmin",
-    "scale": 1.01,
-    "delay": 0.154
-  },
-  {
-    "tx": "24.3vmin",
-    "ty": "-11.5vmin",
-    "scale": 1.25,
-    "delay": 0.011
-  },
-  {
-    "tx": "0.9vw",
-    "ty": "-20.4vh",
-    "scale": 0.9,
-    "delay": 0.106
-  },
-  {
-    "tx": "17.6vmin",
-    "ty": "-31.4vmin",
-    "scale": 1.01,
-    "delay": 0.147
-  },
-  {
-    "tx": "3.2vmin",
-    "ty": "12.4vmin",
-    "scale": 1.01,
+    "tx": "27.6vw",
+    "ty": "-6.6vh",
+    "scale": 1.07,
     "delay": 0.177
   },
   {
-    "tx": "-21.1vw",
-    "ty": "-36.7vh",
-    "scale": 1.12,
-    "delay": 0.133
-  },
-  {
-    "tx": "-4.1vw",
-    "ty": "7.7vh",
-    "scale": 1.17,
-    "delay": 0.151
-  },
-  {
-    "tx": "-16.9vmin",
-    "ty": "-1.4vmin",
-    "scale": 0.97,
-    "delay": 0.17
-  },
-  {
-    "tx": "48.4vw",
-    "ty": "45.7vh",
-    "scale": 1.09,
-    "delay": 0.156
-  },
-  {
-    "tx": "-47.4vw",
-    "ty": "41.4vh",
-    "scale": 1.09,
-    "delay": 0.185
-  },
-  {
-    "tx": "7.1vmin",
-    "ty": "-7.0vmin",
-    "scale": 1.13,
-    "delay": 0.199
-  },
-  {
-    "tx": "12.1vmin",
-    "ty": "9.1vmin",
-    "scale": 1.01,
-    "delay": 0.163
-  },
-  {
-    "tx": "28.3vw",
-    "ty": "23.5vh",
-    "scale": 1.21,
-    "delay": 0.123
-  },
-  {
-    "tx": "-21.0vw",
-    "ty": "-3.9vh",
-    "scale": 1.05,
-    "delay": 0.124
-  },
-  {
-    "tx": "38.2vw",
-    "ty": "3.7vh",
-    "scale": 0.97,
-    "delay": 0.14
-  },
-  {
-    "tx": "-33.3vmin",
-    "ty": "-3.2vmin",
-    "scale": 1.06,
-    "delay": 0.152
-  },
-  {
-    "tx": "-47.9vw",
-    "ty": "-17.4vh",
-    "scale": 0.96,
-    "delay": 0.176
-  },
-  {
-    "tx": "39.7vw",
-    "ty": "-21.5vh",
-    "scale": 1.07,
-    "delay": 0.13
-  },
-  {
-    "tx": "-24.9vmin",
-    "ty": "-25.4vmin",
-    "scale": 1.06,
-    "delay": 0.146
-  },
-  {
-    "tx": "19.8vw",
-    "ty": "-42.1vh",
-    "scale": 1.24,
-    "delay": 0.035
-  },
-  {
-    "tx": "-22.4vmin",
-    "ty": "2.0vmin",
-    "scale": 1.08,
-    "delay": 0.032
-  },
-  {
-    "tx": "-36.6vw",
-    "ty": "9.6vh",
-    "scale": 1.21,
-    "delay": 0.062
-  },
-  {
-    "tx": "-38.5vw",
-    "ty": "-37.1vh",
-    "scale": 0.88,
-    "delay": 0.01
-  },
-  {
-    "tx": "-14.1vw",
-    "ty": "18.6vh",
-    "scale": 1.2,
-    "delay": 0.146
-  },
-  {
-    "tx": "-5.5vmin",
-    "ty": "-0.1vmin",
-    "scale": 1.08,
-    "delay": 0.082
-  },
-  {
-    "tx": "-41.5vw",
-    "ty": "-4.7vh",
-    "scale": 0.96,
-    "delay": 0.108
-  },
-  {
-    "tx": "-28.9vw",
-    "ty": "8.9vh",
-    "scale": 1.2,
-    "delay": 0.145
-  },
-  {
-    "tx": "11.4vmin",
-    "ty": "1.8vmin",
-    "scale": 1.13,
-    "delay": 0.122
-  },
-  {
-    "tx": "27.8vw",
-    "ty": "-19.2vh",
-    "scale": 1.17,
-    "delay": 0.048
-  },
-  {
-    "tx": "12.8vw",
-    "ty": "-34.1vh",
-    "scale": 1.06,
-    "delay": 0.2
-  },
-  {
-    "tx": "23.1vmin",
-    "ty": "-11.5vmin",
-    "scale": 1.07,
-    "delay": 0.138
-  },
-  {
-    "tx": "-28.4vw",
-    "ty": "-24.5vh",
-    "scale": 1.08,
-    "delay": 0.186
-  },
-  {
-    "tx": "24.9vmin",
-    "ty": "10.6vmin",
-    "scale": 0.99,
-    "delay": 0.06
-  },
-  {
-    "tx": "11.8vw",
-    "ty": "36.4vh",
-    "scale": 0.99,
-    "delay": 0.183
-  },
-  {
-    "tx": "22.6vmin",
-    "ty": "15.7vmin",
-    "scale": 1.16,
-    "delay": 0.037
-  },
-  {
-    "tx": "-45.2vw",
-    "ty": "-44.0vh",
-    "scale": 1.04,
-    "delay": 0.209
-  },
-  {
-    "tx": "-26.5vmin",
-    "ty": "-10.4vmin",
-    "scale": 1.13,
-    "delay": 0.129
-  },
-  {
-    "tx": "42.7vw",
-    "ty": "-13.9vh",
+    "tx": "47.3vw",
+    "ty": "20.6vh",
     "scale": 1.14,
-    "delay": 0.142
+    "delay": 0.271
   },
   {
-    "tx": "45.6vw",
-    "ty": "15.9vh",
-    "scale": 1.15,
-    "delay": 0.085
-  },
-  {
-    "tx": "29.5vmin",
-    "ty": "21.0vmin",
-    "scale": 1.14,
-    "delay": 0.206
-  },
-  {
-    "tx": "22.7vw",
-    "ty": "-3.3vh",
-    "scale": 1.18,
-    "delay": 0.082
-  },
-  {
-    "tx": "-36.2vw",
-    "ty": "23.2vh",
-    "scale": 0.93,
-    "delay": 0.073
-  },
-  {
-    "tx": "-25.9vw",
-    "ty": "39.4vh",
-    "scale": 1.06,
-    "delay": 0.116
-  },
-  {
-    "tx": "27.5vmin",
-    "ty": "4.4vmin",
-    "scale": 0.98,
-    "delay": 0.03
-  },
-  {
-    "tx": "-7.9vmin",
-    "ty": "-3.0vmin",
-    "scale": 1.02,
-    "delay": 0.074
-  },
-  {
-    "tx": "-7.7vmin",
-    "ty": "4.1vmin",
-    "scale": 1.05,
-    "delay": 0.079
-  },
-  {
-    "tx": "8.8vw",
-    "ty": "5.6vh",
+    "tx": "-9.0vmin",
+    "ty": "-2.8vmin",
     "scale": 1.0,
-    "delay": 0.209
+    "delay": 0.322
   },
   {
-    "tx": "28.5vmin",
-    "ty": "21.4vmin",
-    "scale": 1.12,
-    "delay": 0.081
+    "tx": "7.8vmin",
+    "ty": "16.1vmin",
+    "scale": 1.23,
+    "delay": 0.251
   },
   {
-    "tx": "10.4vw",
-    "ty": "22.0vh",
-    "scale": 1.15,
-    "delay": 0.055
+    "tx": "17.9vmin",
+    "ty": "-0.7vmin",
+    "scale": 0.94,
+    "delay": 0.302
   },
   {
-    "tx": "-18.9vw",
-    "ty": "47.2vh",
-    "scale": 1.17,
-    "delay": 0.164
-  },
-  {
-    "tx": "-22.6vw",
-    "ty": "25.7vh",
-    "scale": 0.98,
-    "delay": 0.212
-  },
-  {
-    "tx": "-15.3vw",
-    "ty": "-41.0vh",
-    "scale": 0.92,
-    "delay": 0.062
-  },
-  {
-    "tx": "9.2vmin",
-    "ty": "7.7vmin",
-    "scale": 1.15,
-    "delay": 0.067
-  },
-  {
-    "tx": "23.8vw",
-    "ty": "33.6vh",
+    "tx": "9.1vw",
+    "ty": "-33.9vh",
     "scale": 1.07,
+    "delay": 0.236
+  },
+  {
+    "tx": "-1.9vmin",
+    "ty": "-2.4vmin",
+    "scale": 1.05,
+    "delay": 0.345
+  },
+  {
+    "tx": "48.0vw",
+    "ty": "46.5vh",
+    "scale": 1.17,
+    "delay": 0.361
+  },
+  {
+    "tx": "16.4vmin",
+    "ty": "-7.9vmin",
+    "scale": 1.12,
+    "delay": 0.15
+  },
+  {
+    "tx": "34.3vw",
+    "ty": "-6.1vh",
+    "scale": 0.96,
+    "delay": 0.033
+  },
+  {
+    "tx": "-39.8vw",
+    "ty": "-34.0vh",
+    "scale": 1.18,
+    "delay": 0.294
+  },
+  {
+    "tx": "23.1vw",
+    "ty": "8.6vh",
+    "scale": 1.15,
+    "delay": 0.197
+  },
+  {
+    "tx": "-29.9vmin",
+    "ty": "-19.7vmin",
+    "scale": 1.11,
+    "delay": 0.061
+  },
+  {
+    "tx": "-1.6vmin",
+    "ty": "10.0vmin",
+    "scale": 1.16,
+    "delay": 0.333
+  },
+  {
+    "tx": "40.8vw",
+    "ty": "-18.7vh",
+    "scale": 1.23,
+    "delay": 0.283
+  },
+  {
+    "tx": "-20.7vw",
+    "ty": "36.4vh",
+    "scale": 1.0,
+    "delay": 0.255
+  },
+  {
+    "tx": "-27.9vmin",
+    "ty": "-1.1vmin",
+    "scale": 1.12,
+    "delay": 0.084
+  },
+  {
+    "tx": "8.6vw",
+    "ty": "-18.5vh",
+    "scale": 1.06,
+    "delay": 0.279
+  },
+  {
+    "tx": "26.9vmin",
+    "ty": "26.6vmin",
+    "scale": 1.2,
+    "delay": 0.341
+  },
+  {
+    "tx": "-34.0vw",
+    "ty": "36.6vh",
+    "scale": 0.97,
+    "delay": 0.224
+  },
+  {
+    "tx": "13.9vmin",
+    "ty": "14.9vmin",
+    "scale": 1.21,
+    "delay": 0.287
+  },
+  {
+    "tx": "-41.4vw",
+    "ty": "22.7vh",
+    "scale": 1.12,
+    "delay": 0.216
+  },
+  {
+    "tx": "-19.6vmin",
+    "ty": "0.4vmin",
+    "scale": 1.04,
+    "delay": 0.353
+  },
+  {
+    "tx": "2.0vw",
+    "ty": "-45.3vh",
+    "scale": 1.14,
+    "delay": 0.041
+  },
+  {
+    "tx": "-5.7vmin",
+    "ty": "-0.0vmin",
+    "scale": 1.17,
+    "delay": 0.139
+  },
+  {
+    "tx": "12.8vmin",
+    "ty": "-7.9vmin",
+    "scale": 1.12,
+    "delay": 0.31
+  },
+  {
+    "tx": "34.5vw",
+    "ty": "39.3vh",
+    "scale": 1.16,
+    "delay": 0.275
+  },
+  {
+    "tx": "7.5vmin",
+    "ty": "-15.5vmin",
+    "scale": 1.01,
     "delay": 0.107
   },
   {
-    "tx": "-42.2vw",
-    "ty": "-21.3vh",
-    "scale": 1.2,
+    "tx": "26.6vw",
+    "ty": "40.8vh",
+    "scale": 0.91,
+    "delay": 0.123
+  },
+  {
+    "tx": "-1.6vw",
+    "ty": "-10.3vh",
+    "scale": 0.92,
+    "delay": 0.014
+  },
+  {
+    "tx": "-13.0vw",
+    "ty": "32.6vh",
+    "scale": 1.11,
+    "delay": 0.263
+  },
+  {
+    "tx": "-4.5vmin",
+    "ty": "18.8vmin",
+    "scale": 1.14,
+    "delay": 0.213
+  },
+  {
+    "tx": "26.2vmin",
+    "ty": "-12.0vmin",
+    "scale": 1.15,
+    "delay": 0.29
+  },
+  {
+    "tx": "25.8vw",
+    "ty": "-22.1vh",
+    "scale": 1.13,
+    "delay": 0.08
+  },
+  {
+    "tx": "-2.9vw",
+    "ty": "21.5vh",
+    "scale": 1.19,
+    "delay": 0.045
+  },
+  {
+    "tx": "-9.7vmin",
+    "ty": "-5.1vmin",
+    "scale": 0.98,
+    "delay": 0.368
+  },
+  {
+    "tx": "-3.8vmin",
+    "ty": "36.1vmin",
+    "scale": 1.06,
     "delay": 0.065
   },
   {
-    "tx": "38.4vw",
-    "ty": "-36.3vh",
+    "tx": "-0.3vmin",
+    "ty": "-4.3vmin",
+    "scale": 1.2,
+    "delay": 0.17
+  },
+  {
+    "tx": "-41.7vw",
+    "ty": "-9.3vh",
+    "scale": 1.2,
+    "delay": 0.115
+  },
+  {
+    "tx": "37.6vw",
+    "ty": "4.4vh",
+    "scale": 1.13,
+    "delay": 0.131
+  },
+  {
+    "tx": "25.8vmin",
+    "ty": "-17.3vmin",
+    "scale": 1.22,
+    "delay": 0.022
+  },
+  {
+    "tx": "-27.7vw",
+    "ty": "25.4vh",
+    "scale": 1.03,
+    "delay": 0.096
+  },
+  {
+    "tx": "-31.6vmin",
+    "ty": "-7.3vmin",
+    "scale": 1.21,
+    "delay": 0.053
+  },
+  {
+    "tx": "-11.7vmin",
+    "ty": "-7.1vmin",
+    "scale": 0.97,
+    "delay": 0.072
+  },
+  {
+    "tx": "-43.5vw",
+    "ty": "23.1vh",
+    "scale": 1.22,
+    "delay": 0.372
+  },
+  {
+    "tx": "-42.0vw",
+    "ty": "8.4vh",
     "scale": 0.92,
-    "delay": 0.175
+    "delay": 0.24
   },
   {
-    "tx": "-2.8vmin",
-    "ty": "23.0vmin",
-    "scale": 0.91,
-    "delay": 0.056
+    "tx": "8.3vmin",
+    "ty": "6.6vmin",
+    "scale": 1.13,
+    "delay": 0.325
   },
   {
-    "tx": "25.0vw",
-    "ty": "-40.1vh",
-    "scale": 1.15,
-    "delay": 0.079
+    "tx": "3.9vw",
+    "ty": "-23.5vh",
+    "scale": 1.16,
+    "delay": 0.068
   },
   {
-    "tx": "-11.1vmin",
-    "ty": "-4.1vmin",
-    "scale": 1.15,
-    "delay": 0.099
+    "tx": "-15.1vw",
+    "ty": "-41.0vh",
+    "scale": 0.98,
+    "delay": 0.314
+  },
+  {
+    "tx": "19.8vw",
+    "ty": "42.7vh",
+    "scale": 1.2,
+    "delay": 0.193
+  },
+  {
+    "tx": "-21.6vw",
+    "ty": "6.3vh",
+    "scale": 1.13,
+    "delay": 0.181
+  },
+  {
+    "tx": "-46.8vw",
+    "ty": "1.4vh",
+    "scale": 0.99,
+    "delay": 0.142
+  },
+  {
+    "tx": "2.4vw",
+    "ty": "40.9vh",
+    "scale": 1.18,
+    "delay": 0.01
+  },
+  {
+    "tx": "-12.3vw",
+    "ty": "-25.0vh",
+    "scale": 1.04,
+    "delay": 0.174
+  },
+  {
+    "tx": "-21.5vmin",
+    "ty": "-22.3vmin",
+    "scale": 1.04,
+    "delay": 0.092
+  },
+  {
+    "tx": "-13.5vw",
+    "ty": "-7.8vh",
+    "scale": 1.02,
+    "delay": 0.119
+  },
+  {
+    "tx": "-20.1vmin",
+    "ty": "14.8vmin",
+    "scale": 1.03,
+    "delay": 0.329
+  },
+  {
+    "tx": "27.3vw",
+    "ty": "23.6vh",
+    "scale": 0.93,
+    "delay": 0.162
+  },
+  {
+    "tx": "45.6vw",
+    "ty": "-44.0vh",
+    "scale": 1.14,
+    "delay": 0.267
+  },
+  {
+    "tx": "-26.7vw",
+    "ty": "-24.7vh",
+    "scale": 1.19,
+    "delay": 0.376
+  },
+  {
+    "tx": "-5.4vmin",
+    "ty": "-14.1vmin",
+    "scale": 1.19,
+    "delay": 0.135
+  },
+  {
+    "tx": "9.9vw",
+    "ty": "25.8vh",
+    "scale": 0.9,
+    "delay": 0.146
+  },
+  {
+    "tx": "33.5vw",
+    "ty": "25.5vh",
+    "scale": 1.13,
+    "delay": 0.154
+  },
+  {
+    "tx": "-2.0vmin",
+    "ty": "7.8vmin",
+    "scale": 1.22,
+    "delay": 0.037
+  },
+  {
+    "tx": "-3.4vmin",
+    "ty": "5.6vmin",
+    "scale": 1.16,
+    "delay": 0.201
+  },
+  {
+    "tx": "-1.4vmin",
+    "ty": "-27.8vmin",
+    "scale": 1.04,
+    "delay": 0.259
+  },
+  {
+    "tx": "-3.2vmin",
+    "ty": "16.0vmin",
+    "scale": 1.04,
+    "delay": 0.248
+  },
+  {
+    "tx": "-21.5vw",
+    "ty": "-4.1vh",
+    "scale": 0.98,
+    "delay": 0.189
+  },
+  {
+    "tx": "12.1vw",
+    "ty": "7.1vh",
+    "scale": 1.02,
+    "delay": 0.22
+  },
+  {
+    "tx": "37.3vw",
+    "ty": "-37.4vh",
+    "scale": 1.12,
+    "delay": 0.185
+  },
+  {
+    "tx": "-1.6vw",
+    "ty": "48.3vh",
+    "scale": 1.01,
+    "delay": 0.364
+  },
+  {
+    "tx": "-13.9vmin",
+    "ty": "-20.1vmin",
+    "scale": 1.08,
+    "delay": 0.166
+  },
+  {
+    "tx": "-28.5vmin",
+    "ty": "6.8vmin",
+    "scale": 1.11,
+    "delay": 0.244
   }
 ];
